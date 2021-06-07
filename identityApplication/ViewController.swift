0c207c85152cc2ac678257d1ce4880294736bfe9
dissingar djino <dissingardjino@icloud.com>
2021-06-07 17:29:39 +0200

    Initial Commit

---
+++ identityApplication/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  identityApplication
+//
+//  Created by Djino Dissingar on 07/06/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
